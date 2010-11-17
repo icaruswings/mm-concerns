6dd7e3b4677d0736df07c0fce23fb575c44e2df3
Luke Cunningham <luke@icaruswings.com>
2010-11-17 21:56:43 +1100

    first pass at concerns plugin

---
+++ lib/mm-concerns.rb
@@ -0,0 +1,22 @@
+module ConcernsPlugin
+  module ClassMethods
+    def concerned_with(*concerns)
+      concerns.each do |concern|
+        require_dependency "#{name.underscore}/#{concern}"
+      end
+    end
+  end
+  module InstanceMethods
+    def concerned_with(*concerns)
+      self.class.concerned_with(concerns)
+    end
+  end
+end
+
+module ConcernsPluginAddition
+  def self.included(model)
+    model.plugin ConcernsPlugin
+  end
+end
+
+MongoMapper::Document.append_inclusions(ConcernsPluginAddition)
